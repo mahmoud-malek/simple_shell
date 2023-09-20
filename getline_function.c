c28c227e590adca74ed6f7667eb738de54906062
mahmoud <information.mahmoud@gmail.com>
2023-09-20 16:05:16 +0300

    fixed betty

--- getline_function.c
+++ getline_function.c
@@ -1,13 +1,77 @@
 #include "shell.h"
 
+
+/**
+ * read_buffer - is a helper function for _getline
+ * it reads from the file
+ * @fd: file descriptor to read from it
+ * @buffer: to store read data
+ * @buf_ptr: to help know where we stop reading
+ * @buf_size: to update buffer size
+ * Return: 1 on success, 0 otherwise
+ */
+
+static ssize_t read_buffer(int fd, char *buffer, char **buf_ptr,
+						   ssize_t *buf_size)
+{
+	*buf_size = read(fd, buffer, sizeof(buffer) - 1);
+	/*check if buffer was large enough to store chars*/
+	if (*buf_size <= 0)
+	{
+		/*check if have read any data so far*/
+		if (*buf_size == 0)
+			return (-1);
+		/*return number of bytes read*/
+		return (*buf_size);
+	}
+	*buf_ptr = buffer;
+	return (1);
+}
+
+/**
+ * copy_line - helper function for _getline
+ * it copyies spcific bytes from the static buffer
+ * to  the line_ptr for the ueser
+ * @line_ptr: to store line of text in it
+ * @buf_ptr: to know where are we in the static buffer
+ * @total: total number of bytes
+ * @size: size of buffer to return
+ * @bytes_to_cpy: number of bytes to copy to new line
+ * Return: 0 on success, -1 otherwise
+ */
+static ssize_t copy_line(char **line_ptr, char *buf_ptr, ssize_t *total,
+						 size_t *size, ssize_t bytes_to_cpy)
+{
+	/*reallocate space for existing and new number of bytes*/
+	char *new_ptr = _realloc(*line_ptr, *total, *total + bytes_to_cpy + 1);
+
+	if (!new_ptr)
+		return (-1);
+
+	*line_ptr = new_ptr;
+	/*cpy new data from static buffer to line_ptr*/
+	_memcpy(*line_ptr + *total, buf_ptr, bytes_to_cpy);
+	/*update total to be total number of bytes read so far*/
+	*total += bytes_to_cpy;
+	(*line_ptr)[*total] = '\0';
+	*size = *total + 1;
+	return (0);
+}
+
+/**
+ * _getline - reads text from a file and retuns line of text
+ * @line_ptr: line pointer to store the line in it
+ * @size: to store the size of allocated buffer for the line
+ * @fd: file descriptor to read from
+ * Return: number of bytes read or -1 otherwise
+ */
+
 ssize_t _getline(char **line_ptr, size_t *size, int fd)
 {
-	static char buffer[BUF_SIZE];
-	static char *buf_ptr;
+	static char buffer[BUF_SIZE], *buf_ptr;
 	static ssize_t buf_size;
-	ssize_t total = 0, bytes_to_cpy = 0;
-	char *newline_pos = NULL, *new_ptr = NULL;
-
+	ssize_t total = 0, bytes_to_cpy = 0, status = 0;
+	char *newline_pos = NULL;
 
 	if (!line_ptr || !size)
 		return (-1);
@@ -21,38 +85,26 @@ ssize_t _getline(char **line_ptr, size_t *size, int fd)
 		**line_ptr = '\0';
 		*size = 1;
 	}
-
 	do {
 		if (buf_size == 0)
 		{
-			buf_size = read(fd, buffer, sizeof(buffer) - 1);
-			if (buf_size <= 0)
-			{
-				if (buf_size == 0 && total == 0)
-					return (-1);
+			status = read_buffer(fd, buffer, &buf_ptr, &buf_size);
+			if (status == -1)
+				return (-1);
+			if (status <= 0)
 				return (buf_size);
-			}
-			buf_ptr = buffer;
 		}
-
+		/*searches for newline character*/
 		newline_pos = _memchr(buf_ptr, '\n', buf_size);
-		bytes_to_cpy = (newline_pos != NULL
-						 ? newline_pos - buf_ptr + 1
-						 : buf_size);
-
-		new_ptr = _realloc(*line_ptr, total, total + bytes_to_cpy + 1);
-		if (!new_ptr)
+		/*determine how much bytes we need to cpy to get a line*/
+		bytes_to_cpy = (newline_pos != NULL ? newline_pos - buf_ptr + 1 : buf_size);
+		if (copy_line(line_ptr, buf_ptr, &total, size, bytes_to_cpy) == -1)
 			return (-1);
-
-		*line_ptr = new_ptr;
-		_memcpy(*line_ptr + total, buf_ptr, bytes_to_cpy);
-		total += bytes_to_cpy;
-		(*line_ptr)[total] = '\0';
-		*size = total + 1;
-
+		/*move buffer pointer to be after copyied data*/
+		/*decrease buffer size by read bytes*/
 		buf_ptr += bytes_to_cpy;
 		buf_size -= bytes_to_cpy;
+		/*keep loop while not find the newline*/
 	} while (newline_pos == NULL);
-
 	return (total);
 }
--- memory_functions.c
+++ memory_functions.c
@@ -1,5 +1,10 @@
 #include "shell.h"
 
+/**
+ * free_2D - frees a two dimensional array
+ * @array: array to free
+ */
+
 void free_2D(char **array)
 {
 	unsigned int i = 0;
@@ -12,6 +17,14 @@ void free_2D(char **array)
 
 	free(array);
 }
+
+/**
+ * _realloc - reallocate memory for buffer
+ * @buffer: the objective buffer
+ * @old_siz: old size
+ * @new_siz: new size to reallocate
+ * Return: newly allocated space for buffer
+ */
 
 void *_realloc(void *buffer, unsigned int old_siz, unsigned int new_siz)
 {
@@ -39,6 +52,14 @@ void *_realloc(void *buffer, unsigned int old_siz, unsigned int new_siz)
 	return (result);
 }
 
+/**
+ * _memcpy - copyies n bytes from src to dest
+ * @n: number of bytes to copy
+ * @src: srouce buffer
+ * @dest: destination buffer
+ * Return: destination;
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
@@ -48,6 +69,14 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	return (dest);
 }
+
+/**
+ * _memchr - searchs for char in first n bytes in memory
+ * @str: str to search in
+ * @ch: character to search for
+ * @n: number of bytes to search in
+ * Return: pointer to the location where found a char
+ */
 
 void *_memchr(const void *str, int ch, size_t n)
 {
